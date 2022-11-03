bce0b8252fbec3e7faf8182d042b85eff9ff8de9
alanrmendez <alan.mendez07@gmail.com>
2022-11-03 14:13:05 -0400

    revert autoplay refactor

--- index.js
+++ index.js
@@ -2,20 +2,3 @@
 window.onload = function(){
   document.getElementById("home").style.opacity = 1;
 }
-
-
-//trailer autoplay
-$(document).ready(function () {
-  var url = $("#trailerVideo").attr('src');
-
-  $("#trailerVideo").attr('src', '');
-
-  $("#trailerModal").on('show.bs.modal', function () {
-    $("#trailerVideo").attr('src', url);
-  });
-
-  $("#trailerModal").on('hide.bs.modal', function () {
-    $("#trailerVideo").attr('src', '');
-  });
-  
-});
